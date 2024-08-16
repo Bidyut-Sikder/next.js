c135eac0edcbdd24fcc85666e2296a7cc64cb373
Bidyut-Sikder <bidyutsikder2001@gmail.com>
2024-08-16 11:06:35 +0600

    adding

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -3,31 +3,10 @@ import PostContent from "../../components/posts/post-detail/post-content";
 import { getPostData, getPostsFiles } from "../../lib/posts-util";
 import Head from "next/head";
 
-export default function SinglePostDetails(props) {
-  console.log(props);
 
-  return (
-    <Fragment>
-      <Head>
-        <title>{props.post.title}</title>
-        <meta name="description" content={props.post.excerpt} />
-      </Head>
-      <PostContent post={props.post} />
-    </Fragment>
-  );
-}
 
-export async function getStaticPaths() {
-  const postFilesNames = getPostsFiles();
-  const slugs = postFilesNames.map((file) => file.replace(/\.md$/, ""));
 
-  const slugArr = slugs.map((slug) => ({ params: { slug: slug } }));
 
-  return {
-    paths: slugArr,
-    fallback: false, // false or "blocking"
-  };
-}
 
 export async function getStaticProps(context) {
   const { params } = context;
@@ -43,3 +22,32 @@ export async function getStaticProps(context) {
     revalidate: 600,
   };
 }
+
+export async function getStaticPaths() {
+  const postFilesNames = getPostsFiles();
+  const slugs = postFilesNames.map((file) => file.replace(/\.md$/, ""));
+
+  const slugArr = slugs.map((slug) => ({ params: { slug: slug } }));
+
+  return {
+    paths: slugArr,
+    fallback: false, // false or "blocking"
+  };
+}
+
+export default function SinglePostDetails(props) {
+  console.log(props);
+
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.post.title}</title>
+        <meta name="description" content={props.post.excerpt} />
+      </Head>
+      <PostContent post={props.post} />
+    </Fragment>
+  );
+}
+
+
+
